3b4567d9a04a00697f1de4b0c3905a501222f963
Ryan Funduk <ryan@funduk.ca>
2020-08-07 10:52:38 -0400

    support kava-3

--- csir/domain/block.py
+++ csir/domain/block.py
@@ -3,5 +3,12 @@ from csir.utils import clean_timestamp
 
 class Block():
     def __init__(self, data):
-        self.height = int(data['block_meta']['header']['height'])
-        self.timestamp = clean_timestamp(data['block_meta']['header']['time'])
+        self.__data = data
+        self.height = int(self.header()['height'])
+        self.timestamp = clean_timestamp(self.header()['time'])
+
+    def header(self):
+        if 'block_meta' in self.__data:
+            return self.__data['block_meta']['header']
+        else:
+            return self.__data['block']['header']
